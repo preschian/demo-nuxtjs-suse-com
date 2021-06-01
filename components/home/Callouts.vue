ea9af64cc54ccca1a93871435b62e868a086de62
Preschian Febryantara <preschian27@gmail.com>
2021-06-01 12:12:44 +0700

    callouts for desktop

--- components/home/Callouts.vue
+++ components/home/Callouts.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="container pb-6">
+  <div class="container pb-6 lg:flex lg:gap-7 lg:relative lg:-top-32">
     <a
       v-for="item in list"
       :key="item.title"
-      class="mt-6 bg-gray-100 p-6 block"
+      class="mt-6 bg-gray-100 p-6 block lg:flex-1"
       :href="item.link"
       target="_blank"
     >
-      <p class="text-2xl font-medium">{{ item.title }}</p>
-      <p class="mt-3">{{ item.desc }}</p>
-      <p class="mt-3 border-2 p-1 text-center font-medium" :class="item.border">
-        {{ item.cta }}
-      </p>
+      <div class="lg:flex lg:flex-col lg:h-full">
+        <p class="text-2xl font-medium">{{ item.title }}</p>
+        <p class="mt-3 lg:my-3">{{ item.desc }}</p>
+        <p
+          class="mt-3 border-2 p-1 text-center font-medium lg:mt-auto"
+          :class="item.border"
+        >
+          {{ item.cta }}
+        </p>
+      </div>
     </a>
   </div>
 </template>
