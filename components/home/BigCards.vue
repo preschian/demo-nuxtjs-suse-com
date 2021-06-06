b3d161fb1ce3af562c5be3df40ca656cee5a2955
Preschian Febryantara <preschian27@gmail.com>
2021-06-06 16:15:43 +0700

    big cards desktop version

--- components/home/BigCards.vue
+++ components/home/BigCards.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div class="lg:grid lg:grid-cols-2">
     <div
       v-for="(item, index) in items"
       :key="`grid-${index}`"
       :class="item.cssBox"
+      class="lg:p-0"
     >
       <img
         v-if="item.type === 'image'"
@@ -11,17 +12,29 @@
         :alt="item.title"
         class="w-full"
       />
-      <div v-else>
-        <p class="text-3xl font-medium">{{ item.title }}</p>
-        <p class="text-xl mt-6">{{ item.desc }}</p>
-        <a
-          :href="item.cta"
-          target="_blank"
-          class="font-medium mt-6 inline-block border-2 py-1 px-14"
-          :class="item.cssCta"
-        >
-          {{ item.ctaText }}
-        </a>
+      <div
+        v-else
+        class="
+          lg:h-full
+          lg:flex lg:flex-col
+          lg:justify-between
+          container-grid
+          lg:py-12
+        "
+        :class="item.cssBoxGrid"
+      >
+        <p class="text-3xl lg:text-4xl font-medium">{{ item.title }}</p>
+        <div>
+          <p class="text-xl mt-6">{{ item.desc }}</p>
+          <a
+            :href="item.cta"
+            target="_blank"
+            class="font-medium mt-6 inline-block border-2 py-1 px-14"
+            :class="item.cssCta"
+          >
+            {{ item.ctaText }}
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -36,6 +49,9 @@ export default {
           type: 'image',
           src: '/images/home/home01-new.jpg',
           title: 'To infinity, and beyond!',
+          cssBox: {
+            'order-2': true,
+          },
         },
         {
           type: 'box',
@@ -44,9 +60,15 @@ export default {
           cta: '#!',
           ctaText: 'See how',
           cssBox: {
+            'order-1': true,
             'p-6': true,
             'bg-blue': true,
             'text-white': true,
+          },
+          cssBoxGrid: {
+            'lg:ml-auto': true,
+            'lg:pr-12': true,
+            'lg:text-right': true,
           },
           cssCta: {
             'border-white': true,
@@ -57,6 +79,9 @@ export default {
           src: '/images/home/home02.jpg',
           title:
             'Say Goodbye to Downtime for your mission critical SAP solutions',
+          cssBox: {
+            'order-3': true,
+          },
         },
         {
           type: 'box',
@@ -67,6 +92,10 @@ export default {
           ctaText: 'Read more',
           cssBox: {
             'p-6': true,
+            'order-4': true,
+          },
+          cssBoxGrid: {
+            'lg:pl-12': true,
           },
           cssCta: {
             'border-black': true,
@@ -77,6 +106,9 @@ export default {
           src: '/images/home/home03.jpg',
           title:
             'One platform, infinite innovations. Step into the future with SUSE edge Computing.',
+          cssBox: {
+            'order-6': true,
+          },
         },
         {
           type: 'box',
@@ -86,9 +118,15 @@ export default {
           cta: '#!',
           ctaText: 'Learn more',
           cssBox: {
+            'order-5': true,
             'p-6': true,
             'bg-green-light': true,
             'text-black': true,
+          },
+          cssBoxGrid: {
+            'lg:ml-auto': true,
+            'lg:pr-12': true,
+            'lg:text-right': true,
           },
           cssCta: {
             'border-black': true,
